f65975e2b3a41abe78299b0b3d2d3a56fa4a4351
CyanStone <shipeiqing_1991@163.com>
2016-03-27 13:59:26 +0800

    BulbSwicher

--- src/BulbSwitcher.java
+++ src/BulbSwitcher.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class BulbSwitcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(bulbSwitch(new Scanner(System.in).nextInt()));
+	}
+	
+	public static int bulbSwitch(int n) {
+		int result = 0;
+		for(int i = 1; i * i <= n; i++,result++);
+		return result;
 	}
 
 }
