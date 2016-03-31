d46a7aee4fb55ff88dabc043069dd35ddd0442ca
CyanStone <shipeiqing_1991@163.com>
2016-03-31 13:56:14 +0800

    96. Unique Binary Search Trees

--- src/UniqueBinarySearchTrees.java
+++ src/UniqueBinarySearchTrees.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 
 public class UniqueBinarySearchTrees {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		while(in.hasNext()){
+			System.out.println(numTrees(in.nextInt()));
+		}
 	}
 	
-	public int numTrees(int n) {
+	public  static int numTrees(int n) {
+		int [] G = new int[n+1];
+		G[0] = G[1] = 1;
 		
+		for(int i = 2; i <= n; i++){
+			for(int j = 1; j <= i; j++) {
+				G[i] = G[i] + G[j-1] * G[i-j];
+			}
+		}
+		return G[n];
 	}
-
 }
